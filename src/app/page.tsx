85af2c74c0379659367a9fb7a7fa21bcbd06aefa
mingli202 <vincentmingli@gmail.com>
2025-04-26 15:01:20 -0400

    fix other stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "./Hero";
 import NavBar from "./NavBar";
 
 export default function Home() {
   return (
-    <main className="bg-primary/10 min-h-[100dvh] w-[100dvw] overflow-x-hidden">
+    <main className="bg-theme-purple/10 min-h-[100dvh] w-[100dvw] overflow-x-hidden">
       <NavBar />
+      <Hero />
     </main>
   );
 }
