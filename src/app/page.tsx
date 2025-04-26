7edef0ef61dc420067e33287dc99818d321527a2
mingli202 <vincentmingli@gmail.com>
2025-04-26 13:03:09 -0400

    styles and page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import NavBar from "./NavBar";
+
 export default function Home() {
-  return <main className="bg-background">Hello world is htis not</main>;
+  return (
+    <main className="bg-primary/10 min-h-[100dvh] w-[100dvw] overflow-x-hidden">
+      <NavBar />
+    </main>
+  );
 }
