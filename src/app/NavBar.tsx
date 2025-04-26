02348e395caef5929b06e629b90dead1d865c31f
mingli202 <vincentmingli@gmail.com>
2025-04-26 16:51:38 -0400

    feat: auth

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,18 +1,23 @@
+"use client";
+
 import { Button } from "@/ui";
+import { useAuth0 } from "@auth0/auth0-react";
 import { GraduationCap, LogIn } from "lucide-react";
 import Link from "next/link";
 
 export default function NavBar() {
+  const { loginWithRedirect } = useAuth0();
+
   return (
-    <div className="flex w-full items-center justify-between p-5">
-      <div className="flex gap-3 text-2xl font-bold">
+    <div className="flex w-full shrink-0 items-center justify-between p-5">
+      <Link href="/" className="flex gap-3 text-2xl font-bold">
         <GraduationCap className="text-primary h-8 w-8" />
         <div>
           League
           <span className="text-secondary">Of</span>
           Studies
         </div>
-      </div>
+      </Link>
       <div className="flex items-center gap-4">
         <Link href="/dashboard">
           <Button>Dashboard</Button>
@@ -23,14 +28,17 @@ export default function NavBar() {
         <Link href="/leaderboard">
           <Button>Leaderboard</Button>
         </Link>
+
         <Button
-          variant="normal"
+          variant="special"
           className="flex items-center justify-center gap-2"
+          onClick={() => {
+            loginWithRedirect();
+          }}
         >
           <LogIn className="h-4 w-4" />
           Login
         </Button>
-        <Button variant="special">Sign Up</Button>
       </div>
     </div>
   );
