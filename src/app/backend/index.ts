df0bb8e1cc30fe72bb0b741f5f5ff6f51f97db3b
mingli202 <vincentmingli@gmail.com>
2025-04-26 20:28:28 -0400

    feat(backend)!: server actions to access environment variables

---
+++ src/app/backend/index.ts
@@ -0,0 +1,52 @@
+"use server";
+
+import { Database, Tables } from "@/database.types";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient<Database>(process.env.URL!, process.env.ANONKEY!);
+
+export async function getUser(
+  email: string,
+  name: string,
+): Promise<Tables<"users"> | null> {
+  let res = await supabase.from("users").select("*").eq("email", email);
+
+  if (res.data === null) {
+    return null;
+  }
+  console.log("data", res.data);
+
+  if (!res.count || res.count === 0) {
+    await supabase.from("users").insert({ name, email });
+
+    res = await supabase.from("users").select("*").eq("email", email);
+
+    if (res.data === null) {
+      return null;
+    }
+  }
+
+  return res.data[0] satisfies Tables<"users">;
+}
+
+export async function getUserStats(
+  email: string,
+): Promise<Tables<"stats"> | null> {
+  let res = await supabase.from("stats").select("*").eq("email", email);
+
+  if (res.data === null) {
+    return null;
+  }
+
+  if (!res.count || res.count === 0) {
+    await supabase.from("stats").insert({ email });
+
+    res = await supabase.from("stats").select("*").eq("email", email);
+
+    if (res.data === null) {
+      return null;
+    }
+  }
+
+  return res.data[0] satisfies Tables<"stats">;
+}
