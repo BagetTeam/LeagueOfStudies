5a8552d598488bc9b17c4fe432c1305ae9ae4b16
Ewen <ewengue1432@gmail.com>
2025-08-16 23:47:36 -0400

    clean up

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -7,37 +7,23 @@ interface PlayerListProps {
   players: Player[];
   showScores?: boolean;
   currentPlayerId: string;
-  //isResults: boolean;
 }
 
 const PlayerList: React.FC<PlayerListProps> = ({
   players,
   showScores = false,
   currentPlayerId,
-  //isResults = false
 }) => {
-  // Sort players by score if showing scores
   const sortedPlayers = players;
-  // gameMode.type === "equations"
-  //   ? [...players].sort((a, b) => {
-  //       if (a.hasComplete && !b.hasComplete) return -1;
-  //       if (!a.hasComplete && b.hasComplete) return 1;
-
-  //       if (a.hasComplete && b.hasComplete) {
-  //         return a.score - b.score;
-  //       }
-  //       return b.score - a.score;
-  //     })
-  //   : [...players].sort((a, b) => b.score - a.score);
 
   return (
     <div className="space-y-2">
       {sortedPlayers.map((player) => (
         <div
-          key={player.id}
+          key={player.playerId}
           className={cn(
             "bg-background flex items-center gap-2 rounded-lg p-2",
-            player.id === currentPlayerId && "border-primary border-2",
+            player.playerId === currentPlayerId && "border-primary border-2",
             showScores &&
               player.score === Math.max(...players.map((p) => p.score)) &&
               "bg-accent/30",
@@ -48,24 +34,13 @@ const PlayerList: React.FC<PlayerListProps> = ({
           </div>
 
           <div className="flex-grow overflow-hidden">
-            <div className="truncate font-medium">
-              {player.name}
-              {/* {player.isHost && (
-                <span className="text-muted-foreground ml-2 text-xs">
-                  (Host)
-                </span>
-              )} */}
-            </div>
+            <div className="truncate font-medium">{player.name}</div>
           </div>
 
           {showScores && (
             <div className="ml-auto flex items-center">
               <div className="text-xl font-bold">{player.score}</div>
-              <div className="text-muted-foreground ml-1 text-xs">
-                {/* {gameMode.type === "equations" && player.hasComplete
-                  ? "s"
-                  : "pts"} */}
-              </div>
+              <div className="text-muted-foreground ml-1 text-xs"></div>
             </div>
           )}
         </div>
