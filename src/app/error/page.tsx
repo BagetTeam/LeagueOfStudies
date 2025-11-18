272734903b8e4ed004abd37d6846754c3047f2ed
mini-bryanlin <bryanlin316@gmail.com>
2025-11-17 21:47:40 -0500

    user context

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+import { IoReloadOutline } from "react-icons/io5";
+
 export default function ErrorPage() {
-  return <div>Sorry, something went wrong :(</div>;
+  return (
+    <div className="mt-4 flex min-h-screen flex-col items-center justify-start gap-4 p-4 align-top">
+      <p className="text-lg">Sorry, something went wrong :(</p>
+      <button className="bg-theme-purple hover:bg-theme-purple-dark flex w-auto items-center justify-center gap-2 rounded-lg px-6 py-2 font-semibold text-white transition-all">
+        <Link href="/login" className="text-white">
+          Retry Loggging In{" "}
+        </Link>
+        <IoReloadOutline />
+      </button>
+    </div>
+  );
 }
--- src/lib/auth-actions.ts
+++ src/lib/auth-actions.ts
@@ -83,3 +83,4 @@ export async function signInWithGoogle() {
 
   redirect(data.url);
 }
+
