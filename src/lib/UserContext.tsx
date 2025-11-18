0a96296cc14f3f6eb918e2b0de6ac6ed91a03d63
mini-bryanlin <bryanlin316@gmail.com>
2025-11-17 23:48:47 -0500

    more context

--- src/lib/UserContext.tsx
+++ src/lib/UserContext.tsx
@@ -1,1 +1,47 @@
-import { createContext } from "react";
+"use client";
+
+import { createContext, ReactNode, useState, useEffect, useContext } from "react";
+import { User } from "@supabase/supabase-js";
+
+import { createSupClient } from "@/utils/supabase/client";
+
+interface UserContextType {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const supabase = createSupClient();
+    const fetchUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("useUser must be used within an AuthProvider");
+  }
+  return context;
+};
