b868db768b8f25f7a49009e03d3bc1fa6ab21c49
mini-bryanlin <bryanlin316@gmail.com>
2025-11-24 23:17:46 -0500

    notes

--- src/backend/models/database.types.ts
+++ src/backend/models/database.types.ts
@@ -4,141 +4,173 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
   public: {
     Tables: {
       game: {
         Row: {
-          date: string | null
-          email: string
-          id: number
-          mode: string | null
-          result: string | null
-          score: number | null
-          subject: string | null
-          topic: string | null
-        }
+          date: string | null;
+          email: string;
+          id: number;
+          mode: string | null;
+          result: string | null;
+          score: number | null;
+          subject: string | null;
+          topic: string | null;
+        };
         Insert: {
-          date?: string | null
-          email: string
-          id?: number
-          mode?: string | null
-          result?: string | null
-          score?: number | null
-          subject?: string | null
-          topic?: string | null
-        }
+          date?: string | null;
+          email: string;
+          id?: number;
+          mode?: string | null;
+          result?: string | null;
+          score?: number | null;
+          subject?: string | null;
+          topic?: string | null;
+        };
         Update: {
-          date?: string | null
-          email?: string
-          id?: number
-          mode?: string | null
-          result?: string | null
-          score?: number | null
-          subject?: string | null
-          topic?: string | null
-        }
+          date?: string | null;
+          email?: string;
+          id?: number;
+          mode?: string | null;
+          result?: string | null;
+          score?: number | null;
+          subject?: string | null;
+          topic?: string | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "game_email_fkey"
-            columns: ["email"]
-            isOneToOne: false
-            referencedRelation: "stats"
-            referencedColumns: ["email"]
+            foreignKeyName: "game_email_fkey";
+            columns: ["email"];
+            isOneToOne: false;
+            referencedRelation: "stats";
+            referencedColumns: ["email"];
           },
-        ]
-      }
+        ];
+      };
+      notes: {
+        Row: {
+          id: number;
+          email: string | null;
+          tags: Json | null;
+          path: string | null;
+          subject: string | null;
+        };
+        Insert: {
+          id: number;
+          email: string;
+          tags?: Json | null;
+          path?: string | null;
+          subject?: string | null;
+        };
+        Update: {
+          id: number;
+          email: string;
+          tags?: Json | null;
+          path?: string | null;
+          subject?: string | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "notes_email_fkey";
+            columns: ["email"];
+            isOneToOne: false;
+            referencedRelation: "users";
+            referencedColumns: ["email"];
+          },
+        ];
+      };
       stats: {
         Row: {
-          accuracy: number | null
-          email: string
-          level: number | null
-          questionAnswered: number | null
-          questionCorrect: number | null
-          studyStreak: number | null
-          totalGames: number | null
-          totalXp: number | null
-          winRate: number | null
-          wins: number | null
-        }
+          accuracy: number | null;
+          email: string;
+          level: number | null;
+          questionAnswered: number | null;
+          questionCorrect: number | null;
+          studyStreak: number | null;
+          totalGames: number | null;
+          totalXp: number | null;
+          winRate: number | null;
+          wins: number | null;
+        };
         Insert: {
-          accuracy?: number | null
-          email: string
-          level?: number | null
-          questionAnswered?: number | null
-          questionCorrect?: number | null
-          studyStreak?: number | null
-          totalGames?: number | null
-          totalXp?: number | null
-          winRate?: number | null
-          wins?: number | null
-        }
+          accuracy?: number | null;
+          email: string;
+          level?: number | null;
+          questionAnswered?: number | null;
+          questionCorrect?: number | null;
+          studyStreak?: number | null;
+          totalGames?: number | null;
+          totalXp?: number | null;
+          winRate?: number | null;
+          wins?: number | null;
+        };
         Update: {
-          accuracy?: number | null
-          email?: string
-          level?: number | null
-          questionAnswered?: number | null
-          questionCorrect?: number | null
-          studyStreak?: number | null
-          totalGames?: number | null
-          totalXp?: number | null
-          winRate?: number | null
-          wins?: number | null
-        }
+          accuracy?: number | null;
+          email?: string;
+          level?: number | null;
+          questionAnswered?: number | null;
+          questionCorrect?: number | null;
+          studyStreak?: number | null;
+          totalGames?: number | null;
+          totalXp?: number | null;
+          winRate?: number | null;
+          wins?: number | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "stats_email_fkey"
-            columns: ["email"]
-            isOneToOne: true
-            referencedRelation: "users"
-            referencedColumns: ["email"]
+            foreignKeyName: "stats_email_fkey";
+            columns: ["email"];
+            isOneToOne: true;
+            referencedRelation: "users";
+            referencedColumns: ["email"];
           },
-        ]
-      }
+        ];
+      };
       users: {
         Row: {
-          created_at: string
-          email: string
-          name: string | null
-        }
+          created_at: string;
+          email: string;
+          name: string | null;
+        };
         Insert: {
-          created_at?: string
-          email?: string
-          name?: string | null
-        }
+          created_at?: string;
+          email?: string;
+          name?: string | null;
+        };
         Update: {
-          created_at?: string
-          email?: string
-          name?: string | null
-        }
-        Relationships: []
-      }
-    }
+          created_at?: string;
+          email?: string;
+          name?: string | null;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type DefaultSchema = Database[Extract<keyof Database, "public">]
+type DefaultSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
         Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
@@ -146,7 +178,7 @@ export type Tables<
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
   ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
       Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
@@ -154,64 +186,64 @@ export type Tables<
         DefaultSchema["Views"])
     ? (DefaultSchema["Tables"] &
         DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-        Row: infer R
+        Row: infer R;
       }
       ? R
       : never
-    : never
+    : never;
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
   ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Insert: infer I
+        Insert: infer I;
       }
       ? I
       : never
-    : never
+    : never;
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
   ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Update: infer U
+        Update: infer U;
       }
       ? U
       : never
-    : never
+    : never;
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema["Enums"]
     | { schema: keyof Database },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
@@ -219,14 +251,14 @@ export type Enums<
   ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
     ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-    : never
+    : never;
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
@@ -234,10 +266,10 @@ export type CompositeTypes<
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
     ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+    : never;
 
 export const Constants = {
   public: {
     Enums: {},
   },
-} as const
+} as const;
